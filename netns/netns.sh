6c8d18c2bdf1404d96df570e80eef965a063a95f
antonio490 <asc490@hotmail.com>
2023-03-12 20:48:21 +0100

    update netns

--- netns/netns.sh
+++ netns/netns.sh
@@ -32,6 +32,14 @@ ip -n ns2 addr add 10.0.0.2/24 dev veth1
 # routing configuration
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
+# test ping
+ip netns exec ns1 ping 10.0.0.2 -c4
+ip netns exec ns2 ping 10.0.0.1 -c4
+
+
+# remove net namespaces
+ip netns delete ns1
+ip netns delete ns2
 
 # <docs>
 # 1) https://blog.scottlowe.org/2013/09/04/introducing-linux-network-namespaces/
